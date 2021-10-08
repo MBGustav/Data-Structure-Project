f6d88596e4373cf63274f42fffb5826f78e58235
dudubouneto <eduardobouhid.neto@gmail.com>
2021-10-08 14:10:50 -0300

    CSVcreator and CSVreader
    Both functions now tested and successfully working.

--- Release1/main.cpp
+++ Release1/main.cpp
@@ -4,19 +4,44 @@
 #include <fstream>
 using namespace std;
 
-void novoRegistro();
+void novoCadastro(string* nome, string* cpf);
+void CSVcreator(string fPath);
+void CSVreader(string fPath);
 
 int main(){
     //Interface principal
+    /*
     cout << "O que deseja fazer?" << endl;
     cout << "1. Realizar a leitura de presença (criar um novo .csv)" << endl;
     cout << "2. Carregar um arquivo .csv existente" << endl;
-    
+    */
+    cout << "Criação de um arquivo e cadastro de alunos" << endl;
+    CSVcreator("data.csv");
+    CSVreader("data.csv");
 
     return 0;
 
 }
 
+void novoCadastro(string* nome, string* cpf) {
+    cout << "Nome: ";
+    getline(cin, *nome);
+    cout << "CPF: ";
+    getline(cin, *cpf);
+}
+
+/*CSVcreator: cria (ou modifica, caso já exista) um arquivo .csv utilizando o
+método novoCadastro para obter as informações dos participantes.*/
+void CSVcreator(string fPath) {
+    fstream arq;
+    arq.open(fPath, ios::out | ios::app);
+    cout << "===CADASTRO DE PARTICIPANTES===" << endl;
+    string nome, cpf;
+    novoCadastro(&nome, &cpf);
+    //Gravando as informações no arquivo
+    arq << nome << ", ";
+    arq << cpf << ", ";
+}
 
 void CSVreader(string fPath){
 
@@ -28,7 +53,7 @@ void CSVreader(string fPath){
         string celula;
         while (getline(lineStream, celula, ','))
         {
-            //Aqui, temos uma celula na mão!!
+            cout << celula << endl;
         }
         
     }
